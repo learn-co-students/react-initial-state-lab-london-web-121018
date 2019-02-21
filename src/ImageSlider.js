98813541fb2dae1058813038cfd855e45f5022d4
github username <github email address>
2019-02-21 14:08:29 +0000

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,28 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  // changeSlideIndex = () => {
+  //   const newSlideIndex = this.state.currentSlideIndex + 1;
+  //   this.setState({
+  //     currentSlideIndex: newSlideIndex
+  //   })
+  // }
+
+  render() {
+    return (
+      <p>I am on slide {this.state.currentSlideIndex}</p>
+    )
+  }
+
+}
+
+export default ImageSlider;
